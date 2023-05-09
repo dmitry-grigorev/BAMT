a91cabe1708b7d5bbd87e89b7e60079ef40c513b
dmitry-grigorev <crafting20002033@gmail.com>
2023-05-09 15:58:57 +0300

    More statistics are calculated in simulation

--- experiments3/exp3aux/exp3aux.py
+++ experiments3/exp3aux/exp3aux.py
@@ -1,6 +1,6 @@
 import numpy as np
 import networkx as nx
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 import experiments2.auxiliary.auxiliary as ex2aux
 
@@ -9,23 +9,33 @@ import random
 
 
 def simulation(n_features, n_samples=2000, random_state=42, n_trials=20, transitive_mode=False):
-    names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth']
+    names = ['feature_' + str(i + 1) for i in range(n_features)]
     rde_acc, wde_acc = list(), list()
-    for i in range(n_trials):
-        seed = random_state + i
+    asce_acc, dsce_acc = list(), list()
+    wrong_acc = list()
 
+    seed = random_state
+    i = 0
+    while i < n_trials:
         edges, into_degree, out_degree, position = DAGs_generate(n=n_features, max_out=2, random_state=seed)
+        seed += 1
+        if len(edges) == 0:
+            continue
         bn_graph = nx.DiGraph()
         bn_graph.add_edges_from(edges)
-        bn_graph.add_nodes_from(list(range(1, n_features+1)))
+        bn_graph.add_nodes_from(list(range(1, n_features + 1)))
+        if bn_graph.number_of_nodes() != n_features:  # иногда получается больше положенного
+            continue
 
-        #plt.figure()
-        #nx.draw_networkx(bn_graph, arrows=True, pos=position)
-        #plt.show()
+        i += 1
+        # plt.figure()
+        # nx.draw_networkx(bn_graph, arrows=True, pos=position)
+        # plt.show()
 
-        adj_m = set_signs(bn_graph, seed) if not transitive_mode else get_adjacency_matrix_transitive(set_signs(bn_graph, seed), bn_graph)
+        adj_m = set_signs(bn_graph, seed) if not transitive_mode else get_adjacency_matrix_transitive(
+            set_signs(bn_graph, seed), bn_graph)
 
-        true_edges = instantiate_gradation_relation(names, adj_m)
+        true_edges, signs = instantiate_gradation_relation(names, adj_m)
 
         data = build_dataset(adj_m, names, n_samples, seed)
 
@@ -33,10 +43,31 @@ def simulation(n_features, n_samples=2000, random_state=42, n_trials=20, transit
         kbn = kresult['bn']
         kencoder, data_kdiscretized_enc = kresult['encoder'], kresult['disc_data']
 
+        true_edges_ascend, true_edges_descend = [true_edges[i] for i in range(len(true_edges)) if signs[i] > 0], \
+            [true_edges[i] for i in range(len(true_edges)) if signs[i] < 0]
+
         rde, wde = ex2aux.calculate_ratio(kbn.edges, true_edges), ex2aux.calculate_reversed_ratio(kbn.edges, true_edges)
+
+        #print(true_edges_ascend)
+        #print(true_edges_descend)
+
+        # ratio of true edges corresponding to ascending relations in BN among all of those
+        if len(true_edges_ascend) > 0:
+            asce = ex2aux.calculate_ratio(kbn.edges, true_edges_ascend) + ex2aux.calculate_reversed_ratio(kbn.edges,
+                                                                                                          true_edges_ascend)
+            asce_acc.append(asce)
+        # ratio of true edges corresponding to descending relations in BN among all of those
+        if len(true_edges_descend) > 0:
+            dsce = ex2aux.calculate_ratio(kbn.edges, true_edges_descend) + ex2aux.calculate_reversed_ratio(kbn.edges,
+                                                                                                           true_edges_descend)
+            dsce_acc.append(dsce)
+
+        wronge = 1 - ex2aux.calculate_ratio(true_edges, kbn.edges) - ex2aux.calculate_reversed_ratio(true_edges,
+                                                                                                     kbn.edges)
 
         rde_acc.append(rde)
         wde_acc.append(wde)
+        wrong_acc.append(wronge)
 
         del kbn
         del edges
@@ -44,10 +75,21 @@ def simulation(n_features, n_samples=2000, random_state=42, n_trials=20, transit
         del adj_m
         del data
 
-        print(f"Stage {i+1}", end='\r')
-    print("mean, min and max precision on right direction: {:.3f} {:.3f} {:.3f} \n".format(sum(rde_acc)/n_trials, min(rde_acc), max(rde_acc)))
+        print(f"Stage {i + 1}", end='\r')
+    print("mean, min and max precision on right direction: {:.3f} {:.3f} {:.3f} \n".format(sum(rde_acc) / n_trials,
+                                                                                           min(rde_acc), max(rde_acc)))
     print("mean, min and max precision on wrong direction: {:.3f} {:.3f} {:.3f} \n".format(sum(wde_acc) / n_trials,
                                                                                            min(wde_acc), max(wde_acc)))
+    print("mean, min and max precision on ascending relations: {:.3f} {:.3f} {:.3f} \n".format(
+        sum(asce_acc) / len(asce_acc),
+        min(asce_acc), max(asce_acc)))
+    print("mean, min and max precision on descending relations: {:.3f} {:.3f} {:.3f} \n".format(
+        sum(dsce_acc) / len(dsce_acc),
+        min(dsce_acc),
+        max(dsce_acc)))
+    print("mean, min and max precision on wrong edges among all in BN: {:.3f} {:.3f} {:.3f} \n".format(
+        sum(wrong_acc) / n_trials,
+        min(wrong_acc), max(wrong_acc)))
 
 
 def set_signs(G, random_state=42):
@@ -64,25 +106,27 @@ def get_adjacency_matrix_transitive(adj_m, bn_graph):
     n = bn_graph.number_of_nodes()
     M = np.zeros_like(adj_m)
     for J in range(n, 0, -1):
-        #посещаем каждую вершину в обратном обходе
+        # посещаем каждую вершину в обратном обходе
         for j, i in nx.dfs_edges(bn_graph.reverse(), source=J):
             # от каждой обходим граф в глубину для связывания линейных соотношений
             if j == J:
-                M[i-1, J-1] = adj_m[i-1, j-1]
+                M[i - 1, J - 1] = adj_m[i - 1, j - 1]
             for j1, i1 in nx.bfs_edges(bn_graph.reverse(), source=i, depth_limit=1):
                 # перемножаем
-                M[i1-1, J-1] += M[i-1, J-1]*adj_m[i1-1, j1-1]
+                M[i1 - 1, J - 1] += M[i - 1, J - 1] * adj_m[i1 - 1, j1 - 1]
     return M
 
 
 def instantiate_gradation_relation(names, adj_m):
     true_edges = list()
     states = [0, 1, 2]
+    signs = []
     nnzi, nnzj = np.nonzero(adj_m)
     for i, j in zip(nnzi, nnzj):
         sign = int(np.sign(adj_m[i, j]))
         true_edges += ([[names[i] + str(k), names[j] + str(m)] for k, m in zip(states, states[::sign])])
-    return true_edges
+        signs += [sign]*3
+    return true_edges, signs
 
 
 def build_dataset(adj_m, names, n_samples, random_state=42):
